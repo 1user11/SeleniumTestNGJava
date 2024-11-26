b67c8e54e17a10e229a24a4c372dd3987e9e2a5b
1user11 <okepych@yahoo.com>
2024-11-26 08:56:41 -0600

    listeners (#12)
    added listeners

--- src/test/java/com/websitename/tests/TestTitle.java
+++ src/test/java/com/websitename/tests/TestTitle.java
@@ -1,15 +1,16 @@
 package com.websitename.tests;
 
 import com.websitename.base.BaseTest;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class SomeTestExamples extends BaseTest {
+public class TestTitle extends BaseTest {
 
     @Test
     public void testTitle() {
         String expectedTitle= "AskOmDch – Become a Selenium automation expert!";
-        Assert.assertEquals(driver.getTitle(), expectedTitle);
+        Assert.assertEquals(driver.getTitle(), expectedTitle, "Title doesn't match!");
     }
 }
 
