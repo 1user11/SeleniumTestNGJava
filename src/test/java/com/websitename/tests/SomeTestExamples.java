e9ce05679361616d7e0ae11d45e5bd2e85c189af
ok <okepych@yahoo.com>
2024-11-26 01:50:32 -0600

    some updates

--- src/test/java/com/websitename/tests/SomeTestExamples.java
+++ src/test/java/com/websitename/tests/SomeTestExamples.java
@@ -1,16 +1,17 @@
 package com.websitename.tests;
 
 import com.websitename.base.BaseTest;
+import jdk.jfr.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SomeTestExamples extends BaseTest {
 
     @Test
+    @Description("Test Title")
     public void testTitle() {
         String expectedTitle= "AskOmDch – Become a Selenium automation expert!";
         Assert.assertEquals(driver.getTitle(), expectedTitle);
-     // test
     }
 }
 
