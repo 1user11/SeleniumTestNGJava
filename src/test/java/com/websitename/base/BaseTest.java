76e151252d029247bc02372ba310ca580d6a84bd
ok <okepych@yahoo.com>
2024-11-26 08:54:48 -0600

    added listeners

--- src/test/java/com/websitename/base/BaseTest.java
+++ src/test/java/com/websitename/base/BaseTest.java
@@ -1,13 +1,15 @@
 package com.websitename.base;
 
+import com.websitename.listeners.TestListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
 import java.time.Duration;
+import org.testng.annotations.Listeners;
 
+@Listeners(TestListener.class)
 public class BaseTest {
 
     protected WebDriver driver;
@@ -20,10 +22,10 @@ public class BaseTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--disable-gpu");
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
 
         driver = new ChromeDriver(options);
         driver.get("https://askomdch.com/");
-        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
     }
 
