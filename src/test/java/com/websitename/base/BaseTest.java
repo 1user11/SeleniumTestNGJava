dcf69cf5171c814bd553f5f67423065817af2949
ok <okepych@yahoo.com>
2024-12-12 17:32:14 -0600

    update

--- src/test/java/com/websitename/base/BaseTest.java
+++ src/test/java/com/websitename/base/BaseTest.java
@@ -1,12 +1,15 @@
 package com.websitename.base;
 
 import com.websitename.listeners.TestListener;
+import com.websitename.utils.Utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
 import java.time.Duration;
+
 import org.testng.annotations.Listeners;
 
 @Listeners(TestListener.class)
@@ -16,15 +19,19 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--window-size=1920,1080");
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--disable-gpu");
+//        options.addArguments("--remote-allow-origins=*");
+//        options.addArguments("--window-size=1920,1080");
+//
+//        driver = new ChromeDriver(options);
+//        driver.get("https://askomdch.com/");
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
 
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver(Utils.getChromeOptions());
         driver.get("https://askomdch.com/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
     }
