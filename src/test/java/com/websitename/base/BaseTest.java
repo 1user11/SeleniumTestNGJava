daa3398aca569ada7688122d27343939af345c05
ok <okepych@yahoo.com>
2024-12-12 18:51:03 -0600

    update ci

--- src/test/java/com/websitename/base/BaseTest.java
+++ src/test/java/com/websitename/base/BaseTest.java
@@ -19,19 +19,15 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--disable-gpu");
-//        options.addArguments("--remote-allow-origins=*");
-//        options.addArguments("--window-size=1920,1080");
-//
-//        driver = new ChromeDriver(options);
-//        driver.get("https://askomdch.com/");
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--window-size=1920,1080");
 
-        driver = new ChromeDriver(Utils.getChromeOptions());
+        driver = new ChromeDriver(options);
         driver.get("https://askomdch.com/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
     }
